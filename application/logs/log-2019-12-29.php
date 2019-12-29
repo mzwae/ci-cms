0df55979eb146d6f865ed549b7f2a7e070ecfb11
mzwae <mrmonjaie@gmail.com>
2019-12-29 21:23:05 +0000

    Initial commit

---
+++ application/logs/log-2019-12-29.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-12-29 22:04:59 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'shop' C:\xampp7\htdocs\Ecommerce-CodeIgniter\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-12-29 22:04:59 --> Unable to connect to the database
+ERROR - 2019-12-29 22:09:06 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'shop' C:\xampp7\htdocs\Ecommerce-CodeIgniter\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-12-29 22:09:06 --> Unable to connect to the database
