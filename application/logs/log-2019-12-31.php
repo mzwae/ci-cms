9a38ce7b7a65706e9184272e5318fa55b4db79f9
mzwae <mrmonjaie@gmail.com>
2019-12-31 21:24:04 +0000

    Fixed js and css loading bug

---
+++ application/logs/log-2019-12-31.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-12-31 22:22:13 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:22:13 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:22:13 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:22:14 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:22:34 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:22:35 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:22:35 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:22:35 --> 404 Page Not Found: /index
