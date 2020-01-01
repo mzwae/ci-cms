610d399cc10f531b64c04a2522e91740c8aeac1f
mzwae <mrmonjaie@gmail.com>
2020-01-01 17:41:42 +0000

    Fixed no logo bug

--- application/logs/log-2019-12-31.php
+++ application/logs/log-2019-12-31.php
@@ -8,3 +8,11 @@ ERROR - 2019-12-31 22:22:34 --> 404 Page Not Found: /index
 ERROR - 2019-12-31 22:22:35 --> 404 Page Not Found: /index
 ERROR - 2019-12-31 22:22:35 --> 404 Page Not Found: /index
 ERROR - 2019-12-31 22:22:35 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:24:29 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:24:33 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:24:36 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:24:40 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:24:48 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:25:01 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:25:04 --> 404 Page Not Found: /index
+ERROR - 2019-12-31 22:25:09 --> 404 Page Not Found: /index
---
+++ application/logs/log-2020-01-01.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-01-01 18:37:14 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:37:28 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:37:47 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:37:47 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:37:48 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:04 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:05 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:05 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:05 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:12 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:13 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:13 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:13 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:24 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:24 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:25 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:39:25 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:40:18 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:40:18 --> 404 Page Not Found: /index
+ERROR - 2020-01-01 18:40:18 --> 404 Page Not Found: /index
